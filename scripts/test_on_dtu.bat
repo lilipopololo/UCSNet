67c2e66d1c7a25ea1d26c786433f154e3a49a5a0
lilipopololo <996543105@qq.com>
2021-04-07 11:33:43 +0800

    4-7

--- scripts/test_on_dtu.bat
+++ scripts/test_on_dtu.bat
@@ -8,4 +8,4 @@ root_path="E:/dataset/dtu_test/dtu"
 
 
 
-CUDA_VISIBLE_DEVICES=0 python test.py --root_path $root_path --test_list $test_list --save_path $save_path --max_h 1200 --max_w 1600
+python test.py --root_path $root_path --test_list $test_list --save_path $save_path --max_h 1200 --max_w 1600
